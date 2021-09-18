aea5c91d132f4edcc7261a3e4a5dfc9e7c60ae9e
gusdyd98 <gusdyd98@gmail.com>
2021-09-19 00:31:17 +0900

    Chapter 7. 진행 중

--- main_chapter7.py
+++ main_chapter7.py
@@ -125,12 +125,317 @@ ndf=df[['mpg', 'cylinders', 'horsepower', 'weight']]
 X=ndf[['weight']]
 y=ndf['mpg']
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test=train_test_split(X,
-                                                  y,
-                                                  test_size=0.3,
-                                                  random_state=10)
+print('훈련 데이터: ', X_train.shape)
+print('검증 데이터: ', X_train.shape)
 
-print('train data : ',X_train.shape)
-print('test data : ', X_test.shape)
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+poly=PolynomialFeatures(degree=2)
+X_train_poly=poly.fit_transform(X_train)
+
+print('원 데이터 : ', X_train.shape)
+print('2차향 변환 데이터 : ', X_train_poly.shape)
+
+pr=LinearRegression()
+pr.fit(X_train_poly, y_train)
+
+X_test_poly=poly.fit_transform(X_test)
+r_square=pr.score(X_test_poly, y_test)
+print(r_square)
+
+
+y_hat_test=pr.predict(X_test_poly)
+
+fig=plt.figure(figsize=(10,5))
+ax=fig.add_subplot(1,1,1)
+ax.plot(X_train, y_train, 'o', label='Train Data')
+ax.plot(X_test, y_hat_test, 'r+', label='Predicted Value')
+ax.legend(loc='best')
+plt.xlabel('weight')
+plt.ylabel('mpg')
+plt.show()
+plt.close()
+
+
+
+X_poly=poly.fit_transform(X)
+y_hat=pr.predict(X_poly)
+
+plt.figure(figsize=(10,5))
+ax1=sns.distplot(y    , hist=False, label='y'            )
+ax2=sns.distplot(y_hat, hist=False, label='y_hat', ax=ax1)
+plt.show()
+plt.close()
+
+
+print('##########################################################')
+print('## 예제7-3. 다중회귀분석                                   ')
+print('##########################################################')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('./part7/auto-mpg.csv', header=None)
+
+df.columns=['mpg','cylinders','displacement', 'horsepower', 'weight',
+            'acceleration', 'model year', 'origin', 'name']
+
+df['horsepower'].replace('?', np.nan, inplace=True)
+df.dropna(subset=['horsepower'], axis=0, inplace=True)
+df['horsepower']=df['horsepower'].astype('float')
+
+ndf=df[['mpg', 'cylinders', 'horsepower', 'weight']]
+
+X=ndf[['cylinders', 'horsepower', 'weight']]
+y=ndf['mpg']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+
+print('훈련 데이터 : ', X_train.shape)
+print('검증 데이터 : ', X_test .shape)
+
+
+print('##########################################################')
+print('## 예제7-4. KNN (k-Nearest-Neighbors) 분류 알고리즘          ')
+print('##########################################################')
+import pandas as pd
+import seaborn as sns
+
+df=sns.load_dataset('titanic')
+
+print(df.head())
+
+pd.set_option('display.max_columns', 15)
+print(df.head())
+print(df.info())
+
+rdf=df.drop(['deck', 'embark_town'], axis=1)
+print(rdf.columns.values)
+
+rdf=rdf.dropna(subset=['age'], how='any', axis=0)
+print(len(rdf))
+
+most_freq=rdf['embarked'].value_counts(dropna=True).idxmax()
+print(most_freq)
+print('\n')
+
+print(rdf.describe(include='all'))
+print('\n')
+
+rdf['embarked'].fillna(most_freq, inplace=True)
+
+ndf=rdf[['survived', 'pclass', 'sex', 'age', 'sibsp', 'parch', 'embarked']]
+print(ndf.head())
+
+onehot_sex=pd.get_dummies(ndf['sex'])
+print(onehot_sex)
+
+ndf=pd.concat([ndf, onehot_sex], axis=1)
+print(ndf.head())
+
+onehot_embarked=pd.get_dummies(ndf['embarked'], prefix='town')
+print(onehot_embarked)
+
+ndf=pd.concat([ndf, onehot_embarked], axis=1)
+print(ndf.head())
+
+
+ndf.drop(['sex', 'embarked'], axis=1, inplace=True)
+print(ndf.head())
+
+X=ndf[['pclass', 'age', 'sibsp', 'parch', 'female', 'male',
+       'town_C', 'town_Q', 'town_S']]
+y=ndf['survived']
+
+from sklearn import preprocessing
+X=preprocessing.StandardScaler().fit(X).transform(X)
+
+print(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.3, random_state=10)
+
+print('train data 개수: ', X_train.shape)
+print('test  data 개수: ', X_test.shape)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn=KNeighborsClassifier(n_neighbors=5)
+
+knn.fit(X_train, y_train)
+
+y_hat=knn.predict(X_test)
+
+print(y_hat[0:10])
+print(y_test.values[0:10])
+
+
+from sklearn import metrics
+knn_matrix=metrics.confusion_matrix(y_test, y_hat)
+print(knn_matrix)
+
+knn_report=metrics.classification_report(y_test, y_hat)
+print(knn_report)
+
+
+
+print('##########################################################')
+print('## 예제7-5. SVM 모형           ')
+print('##########################################################')
+import pandas as pd
+import seaborn as sns
+
+df=sns.load_dataset('titanic')
+
+print(df.head())
+
+pd.set_option('display.max_columns', 15)
+print(df.head())
+print(df.info())
+
+rdf=df.drop(['deck', 'embark_town'], axis=1)
+print(rdf.columns.values)
+
+rdf=rdf.dropna(subset=['age'], how='any', axis=0)
+print(len(rdf))
+
+most_freq=rdf['embarked'].value_counts(dropna=True).idxmax()
+print(most_freq)
+print('\n')
+
+print(rdf.describe(include='all'))
+print('\n')
+
+rdf['embarked'].fillna(most_freq, inplace=True)
+
+ndf=rdf[['survived', 'pclass', 'sex', 'age', 'sibsp', 'parch', 'embarked']]
+print(ndf.head())
+
+onehot_sex=pd.get_dummies(ndf['sex'])
+print(onehot_sex)
+
+ndf=pd.concat([ndf, onehot_sex], axis=1)
+print(ndf.head())
+
+onehot_embarked=pd.get_dummies(ndf['embarked'], prefix='town')
+print(onehot_embarked)
+
+ndf=pd.concat([ndf, onehot_embarked], axis=1)
+print(ndf.head())
+
+
+ndf.drop(['sex', 'embarked'], axis=1, inplace=True)
+print(ndf.head())
+
+X=ndf[['pclass', 'age', 'sibsp', 'parch', 'female', 'male',
+       'town_C', 'town_Q', 'town_S']]
+y=ndf['survived']
+
+from sklearn import preprocessing
+X=preprocessing.StandardScaler().fit(X).transform(X)
+
+print(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.3, random_state=10)
+
+print('train data 개수: ', X_train.shape)
+print('test  data 개수: ', X_test.shape)
+
+
+from sklearn import svm
+
+svm_model=svm.SVC(kernel='rbf')
+
+svm_model.fit(X_train, y_train)
+
+y_hat=svm_model.predict(X_test)
+
+print(y_hat[0:10])
+print(y_test.values[0:10])
+
+from sklearn import metrics
+
+svm_matrix=metrics.confusion_matrix(y_test, y_hat)
+print(svm_matrix)
+print('\n')
+
+svm_report=metrics.classification_report(y_test, y_hat)
+print(svm_report)
+
+
+
+print('##########################################################')
+print('## 예제7-6. Decision Tree 모형           ')
+print('##########################################################')
+import pandas as pd
+import numpy as np
+
+uci_path='https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+df=pd.read_csv(uci_path, header=None)
+
+df.columns=['id', 'clump', 'cell_size', 'cell_shape', 'adhesion', 'epithlial',
+            'bare_nuclei', 'chromatin', 'normal_nucleoli', 'mitoses', 'class']
+
+pd.set_option('display.max_columns', 15)
+
+print(df.head())
+print('\n')
+
+print(df.info())
+print('\n')
+
+print(df.describe())
+
+print(df['bare_nuclei'].unique())
+print('\n')
+
+df['bare_nuclei'].replace('?', np.nan, inplace=True)
+df.dropna(subset=['bare_nuclei'], axis=0, inplace=True)
+df['bare_nuclei']=df['bare_nuclei'].astype('int')
+
+print(df.describe())
+
+
+X=df[['clump', 'cell_size', 'cell_shape', 'adhesion', 'epithlial',
+      'bare_nuclei', 'chromatin', 'normal_nucleoli', 'mitoses']]
+y=df['class']
+
+from sklearn import preprocessing
+X=preprocessing.StandardScaler().fit(X).transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+
+print('train data 개수 : ', X_train.shape)
+print('test  data 개수 : ', X_test.shape)
+
+from sklearn import tree
+
+tree_model=tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
+
+tree_model.fit(X_train, y_train)
+
+y_hat=tree_model.predict(X_test)
+
+
+print(y_hat[0:10])
+print(y_test.values[0:10])
+
+from sklearn import metrics
+
+tree_matrix=metrics.confusion_matrix(y_test, y_hat)
+print(tree_matrix)
+print('\n')
+
+tree_report=metrics.classification_report(y_test, y_hat)
+print(tree_report)
+
 
