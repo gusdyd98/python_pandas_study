bd3a653a575397bc7db93e3b65dbf63f0a85e068
gusdyd98 <gusdyd98@gmail.com>
2021-09-19 09:22:35 +0900

    Chapter 7. 7-8 진행 중

--- main_chapter7.py
+++ main_chapter7.py
@@ -439,3 +439,84 @@ tree_report=metrics.classification_report(y_test, y_hat)
 print(tree_report)
 
 
+print('##########################################################')
+print('## 예제7-7. k-means 군집 분석            ')
+print('##########################################################')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+uci_path='https://archive.ics.uci.edu/ml/machine-learning-databases/\
+00292/Wholesale%20customers%20data.csv'
+df=pd.read_csv(uci_path, header=0)
+
+print(df.head())
+print('\n')
+
+print(df.info())
+print('\n')
+
+print(df.describe())
+
+X=df.iloc[:, :]
+print(X[:5])
+print('\n')
+
+from sklearn import preprocessing
+X = preprocessing.StandardScaler().fit(X).transform(X)
+
+print(X[:5])
+
+from sklearn import cluster
+
+kmeans=cluster.KMeans(init='k-means++', n_clusters=5, n_init=10)
+
+kmeans.fit(X)
+
+cluster_label=kmeans.labels_
+print(cluster_label)
+print('\n')
+
+df['Cluster']=cluster_label
+print(df.head())
+
+
+df.plot(kind='scatter', x='Grocery', y='Frozen', c='Cluster', cmap='Set1',
+        colorbar=False, figsize=(10,10))
+df.plot(kind='scatter', x='Milk', y='Delicassen', c='Cluster', cmap='Set1',
+        colorbar=True, figsize=(10,10))
+
+plt.show()
+plt.close()
+plt.close()
+
+mask=(df['Cluster']==0) | (df['Cluster']==4)
+ndf=df[~mask]
+
+
+ndf.plot(kind='scatter', x='Grocery', y='Frozen', c='Cluster', cmap='Set1',
+        colorbar=False, figsize=(10,10))
+ndf.plot(kind='scatter', x='Milk', y='Delicassen', c='Cluster', cmap='Set1',
+        colorbar=True, figsize=(10,10))
+
+plt.show()
+plt.close()
+plt.close()
+
+
+print('##########################################################')
+print('## 예제7-8. DBSCAN  군집 분석            ')
+print('##########################################################')
+import pandas as pd
+import folium
+
+file_path='./part7/2016_middle_shcool_graduates_report.xlsx'
+
+df=pd.read_excel(file_path, header=0)
+
+pd.set_option('display.width', None)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_colwidth', 20)
+pd.set_option('display.unicode.east_asian_width', True)
+
+print(df.columns.values)
